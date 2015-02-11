94e18fc6d92f4d8c110f6e4303fa41938f59deac
Mihyaeru <mihyaeru21@gmail.com>
2015-02-12 01:12:39 +0900

    Add pod

--- lib/WebService/Slack/WebApi.pm
+++ lib/WebService/Slack/WebApi.pm
@@ -36,3 +36,98 @@ for my $class_name (qw/ api auth channels chat emoji files groups im oauth rtm s
 
 1;
 
+__END__
+=pod
+
+=head1 NAME
+
+WebService::Slack::WebApi - a simple wrapper for Slack Web API
+
+=head1 SYNOPSIS
+
+    use WebService::Slack::WebApi;
+
+    # the token is required unless using $slack->oauth->access
+    my $slack = WebService::Slack::WebApi->new(token => 'access token');
+
+    # getting channel's descriptions
+    my $channels = $slack->channels->list;
+
+    # posting message to specified channel and getting message description
+    my $posted_message = $slack->chat->post_message(
+        channel  => 'channel id', # required
+        text     => 'hoge',       # required
+        username => 'fuga',       # optional
+        # othre optional parameters...
+    );
+
+=head1 DESCRIPTION
+
+WebService::Slack::WebApi is a simple wrapper for Slack Web API (https://api.slack.com/web).
+
+
+=head1 METHODS
+
+This module provides all methods declared in the API reference (https://api.slack.com/methods).
+
+=head2 Basis
+
+C<WebService::Slack::WebApi::Namespace::method_name> corresponds to C<namespace.methodName> in Slack Web API.
+For example C<WebService::Slack::WebApi::Chat::post_message> corresponds to C<chat.postMessage>.
+You describe as below to call C<Chat::post_message> method.
+
+    my $result = $slack->chat->post_message;
+
+=head2 The token parameter
+
+The API reference shows C<chat.update> method require 4 parameters: C<token>, C<ts>, C<channel> and C<text>.
+When using this module C<token> parameter is added implicitly except using C<oauth.access> method.
+So you pass the other 3 parameters to C<Chat::update> method as shown below.
+
+    my $result = $slack->chat->update(
+        ts      => '1401383885.000061',  # as Str
+        channel => 'channel id',
+        text    => 'hoge',
+    );
+
+=head2 Optional parameters
+
+Some methods have optional parameters.
+If a parameter is optional in the API reference, it is also optional in this module.
+
+=head2 Not primitive parameters
+
+These parameters are not primitive:
+
+=over
+
+=item C<files.upload.file>: string of path to local file
+
+=item C<files.upload.channels>: ArrayRef of channel id string
+
+=back
+
+=head1 SEE ALSO
+
+=over
+
+=item https://api.slack.com/web
+
+=item https://api.slack.com/methods
+
+=back
+
+=head1 AUTHOR
+
+mihyaeru21 E<lt>mihyaeru21@gmail.comE<gt>
+
+=head1 LICENSE
+
+Copyright (C) mihyaeru21
+
+Released under the MIT license.
+
+See C<LICENSE> file.
+
+=cut
+
